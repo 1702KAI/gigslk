9c16042ca793f6b7f8337168eff78eb9b2ad6172
ImethVinnath <im3th.v@gmail.com>
2023-11-20 15:05:16 +0530

    Implemented backend for the designed features on frontend

--- gigslk/app/Models/User.php
+++ gigslk/app/Models/User.php
@@ -27,6 +27,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'address',
+        'job_title',
+        'company_name',
     ];
 
     /**
