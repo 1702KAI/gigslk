baee127e2ffab20b783b4c682e34e024797d1908
ImethVinnath <im3th.v@gmail.com>
2023-12-20 22:58:19 +0530

    added a migration file

---
+++ gigslk/database/migrations/2023_12_20_172722_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('mobile_number')->nullable();
+            $table->decimal('hourly_rate', 8, 2)->nullable();
+            $table->text('tags')->nullable();
+            $table->text('bio')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('mobile_number');
+            $table->dropColumn('hourly_rate');
+            $table->dropColumn('tags');
+            $table->dropColumn('bio');
+        });
+    }
+}
